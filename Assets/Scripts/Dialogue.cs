c1a5ba2625e62de5a38fc452e7d6753eed99caf6
tjdqls005539 <128605122+tjdqls005539@users.noreply.github.com>
2025-07-17 02:36:37 +0900

    2025-07-17

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static DialogueList;
+
+[System.Serializable] //직접 만든 class에 접근할 수 있도록 해줌. 
+public class DialogueList
+{
+    public enum dialoguename
+    { 
+        청운객,
+        흑혈수라,
+        나레이션
+    }
+    [SerializeField]
+    public dialoguename names;
+    [TextArea]
+    public string dialogue;
+}
+public class Dialogue : MonoBehaviour
+{
+    public DialogueList[] dialogue;
+    TextMeshProUGUI text;
+    TextMeshProUGUI nametext;
+    GameObject chat_Box;
+    GameObject next_Box;
+    GameObject skip_Box;
+    GameObject enemy_Image;
+    private int count = 0;
+    private float printSpeed = 0.025f;
+    private bool end = false;
+    private bool next = true;
+
+    void Start()
+    {
+        //Debug.Log(); //Debug
+        text = this.GetComponent<TextMeshProUGUI>();
+        nametext = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        enemy_Image = GameObject.Find("EnemyImage");
+        chat_Box = transform.GetChild(1).gameObject;
+        next_Box = transform.GetChild(2).gameObject;
+        skip_Box = transform.GetChild(3).gameObject;
+        StartBox();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && next)
+        {
+            next_Box.SetActive(false);
+            next = false;
+            if (end)
+            {
+                EndDialogue();
+            }
+            else
+            {
+                text.text = "";
+                chat_Box.SetActive(true);
+                skip_Box.SetActive(true);
+                StartCoroutine(TextPrint(printSpeed));
+            }
+        }
+    }
+    IEnumerator TextPrint(float d)
+    {
+        int corou_count = 0;
+        nametext.text = dialogue[count].names.ToString();
+        while (corou_count != dialogue[count].dialogue.Length)
+        {
+            if (corou_count < dialogue[count].dialogue.Length)
+            {
+                text.text += dialogue[count].dialogue[corou_count].ToString();
+                corou_count++;
+            }
+            yield return new WaitForSeconds(printSpeed);
+        }
+        next_Box.SetActive(true);
+        if (count < dialogue.Length - 1) count++;
+        else end = true;
+        next = true;
+    }
+
+// 스킵 버튼 누를 시 게임 씬으로 이동
+    public void EndDialogue()
+    {
+        ResetDialogue();
+        StopAllCoroutines();
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+    public void ResetDialogue()
+    {
+        text.text = "";
+        nametext.text = "";
+        chat_Box.SetActive(false);
+        next_Box.SetActive(false);
+        skip_Box.SetActive(false);
+        enemy_Image.SetActive(false);
+
+    }
+    public void StartBox()
+    {
+        chat_Box.SetActive(false);
+        next_Box.SetActive(false);
+        skip_Box.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform enemyContainer;
+    public TMP_Text comboText;
+    private int comboCount = 0;
+    public GameObject GameOverUI;
+    public GameObject GameClearUI;
+
+    public float beatInterval = 5f;
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+
+    private string[] directions = { "Up", "Left", "Down", "Right" };
+    private string currentDirection = "";
+
+    private bool canInput = false;
+    private bool gameover = false;
+    private bool gameclear = false;
+
+    private enum Result { None, Success, Fail }
+    private Result resultState = Result.None;
+
+    void Start()
+    {
+        comboText.text = "";
+        GameOverUI.SetActive(false);
+        GameClearUI.SetActive(false);
+        StartCoroutine(PlayLoop());
+    }
+
+    void Update()
+    {
+        Debug.Log(gameclear);
+        if (!canInput || gameover) return;
+
+        KeyCode correctKey = GetKeyForDirection(currentDirection);
+
+
+        if (Input.GetKeyDown(correctKey))
+        {
+            resultState = Result.Success;
+            canInput = false;
+
+            comboCount++;
+            comboText.text = "Combo: " + comboCount;
+
+            Debug.Log("성공 입력: " + correctKey);
+        }
+
+        else if (Input.anyKeyDown && !Input.GetKeyDown(correctKey))
+        {
+            resultState = Result.Fail;
+            canInput = false;
+
+            Debug.Log("틀린 키 입력");
+        }
+    }
+
+    IEnumerator PlayLoop()
+    {
+        while (!gameover || !gameclear)
+        {
+            currentDirection = directions[Random.Range(0, directions.Length)];
+            Visible_Enemy(currentDirection);
+
+            resultState = Result.None;
+            canInput = true;
+
+            float timer = 0f;
+
+            while (timer < beatInterval)
+            {
+                if (resultState == Result.Success)
+                    break;
+
+                if (resultState == Result.Fail)
+                {
+                    StartCoroutine(GameOver());
+                    yield break;
+                }
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            canInput = false;
+
+            if (resultState == Result.None)
+            {
+                Debug.Log("실패: 시간 초과");
+                StartCoroutine(GameOver());
+                yield break;
+            }
+
+            if(comboCount >= 5)
+            {
+                StartCoroutine(GameClear());
+                yield break;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+            Invisible_Enemy();
+        }
+    }
+
+    IEnumerator GameOver()
+    {
+        if (gameover) yield break;
+
+        gameover = true;
+        GameOverUI.SetActive(true);
+        Invisible_Enemy();
+        yield return null;
+    }
+
+    IEnumerator GameClear()
+    {
+        if (gameclear) yield break;
+        gameclear = true;
+        
+        GameClearUI.SetActive(true);
+        Invisible_Enemy();
+        SceneManager.LoadScene("ChattingScene");
+    }
+
+    void Visible_Enemy(string direction)
+    {
+        foreach (Transform child in enemyContainer)
+        {
+            child.gameObject.SetActive(child.name == "Enemy_" + direction);
+        }
+    }
+
+    void Invisible_Enemy()
+    {
+        foreach (Transform child in enemyContainer)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    KeyCode GetKeyForDirection(string dir)
+    {
+        return dir switch
+        {
+            "Up" => upKey,
+            "Down" => downKey,
+            "Left" => leftKey,
+            "Right" => rightKey,
+            _ => KeyCode.None
+        };
+    }
+    public void Regame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ReMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
